046adeb50021ea3edd045f4315fa1a0b0732b63c
Garrett Baker <garrett.baker@exacttarget.com>
2016-06-22 18:38:02 -0400

    renamed twentysixteen to foundation

--- wp-content/themes/foundation/index.php
+++ wp-content/themes/foundation/index.php
@@ -10,8 +10,8 @@
  * @link http://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
- * @subpackage Twenty_Sixteen
- * @since Twenty Sixteen 1.0
+ * @subpackage Foundation
+ * @since Foundation 1.0
  */
 
 get_header(); ?>
@@ -43,9 +43,9 @@ get_header(); ?>
 
 			// Previous/next page navigation.
 			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentysixteen' ),
-				'next_text'          => __( 'Next page', 'twentysixteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
+				'prev_text'          => __( 'Previous page', 'foundation' ),
+				'next_text'          => __( 'Next page', 'foundation' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'foundation' ) . ' </span>',
 			) );
 
 		// If no content, include the "No posts found" template.
