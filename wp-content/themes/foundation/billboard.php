88f3187acf6ebafadb7f6c4cd017082900e29c5c
Garrett Baker <garrett.baker@exacttarget.com>
2016-06-27 21:31:18 -0400

    added billboard template part

---
+++ wp-content/themes/foundation/billboard.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The home template file
+ *
+ * This file renders the homepage.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Foundation
+ * @since Foundation 1.0
+ */
+
+<div class="billboard"></div>
--- wp-content/themes/foundation/index.php
+++ wp-content/themes/foundation/index.php
@@ -19,6 +19,14 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		<?php
+			if ( is_home() && is_front_page() ) :
+
+				get_template_part( 'billboard' );
+
+			endif;
+		?>
+
 		<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
