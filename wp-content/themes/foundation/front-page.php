52033cc743530fe07510ad15c7f092ebe84f14a9
Garrett Baker <garrett.baker@exacttarget.com>
2016-06-29 19:40:26 -0400

    added video

--- wp-content/themes/foundation/front-page.php
+++ wp-content/themes/foundation/front-page.php
@@ -15,7 +15,15 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		    <div class="billboard"></div>
+		    <div class="billboard">
+                <div class="video-bg">
+            		<video autoplay muted loop poster="<?php get_template_directory_uri(); ?>/files/busy-new-york-intersection.jpg">
+            			<source src="<?php get_template_directory_uri(); ?>/files/busy-new-york-intersection.mp4" type="video/mp4">
+            			<source src="<?php get_template_directory_uri(); ?>/files/busy-new-york-intersection.webm" type="video/webm">
+            			<source src="<?php get_template_directory_uri(); ?>/files/busy-new-york-intersection.ogv" type="video/ogg">
+            		</video>
+            	</div>
+            </div>
 
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
