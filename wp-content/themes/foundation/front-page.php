27365710309d2779ae80fb701b37634478389e24
Garrett Baker <garrett.baker@gmail.com>
2016-07-06 21:26:23 -0400

    donate button styles

--- wp-content/themes/foundation/front-page.php
+++ wp-content/themes/foundation/front-page.php
@@ -16,6 +16,7 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		    <div class="billboard">
+				<h1 class="billboard-title">Where Learning Becomes Life</h1>
                 <div class="video-bg">
             		<video autoplay muted loop poster="<?php echo get_template_directory_uri(); ?>/files/busy-new-york-intersection.jpg">
             			<source src="<?php echo get_template_directory_uri(); ?>/files/busy-new-york-intersection.mp4" type="video/mp4">
